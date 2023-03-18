c180276b9f6a7ca244daeec18460095c24a2e65b
aditibhambure <113377275+aditibhambure@users.noreply.github.com>
2023-03-18 11:02:49 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,20 @@
+class Ifelseladder{
+
+public static void main(String[] args){
+String name= "Aditi";
+
+if(name=="Reena"){
+	System.out.println("Name is Reena");
+}
+else if(name=="Jiya"){
+	System.out.println("Name is Jiya");
+}
+else if(name=="Ajit"){
+       System.out.println("Name is Ajit");
+}
+
+else{
+	System.out.println(name);
+}
+}
+}
